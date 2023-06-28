33450e737ec3cdb0bca43eb0f027957ffb186c5e
iacswhat <111007847+iacswhat@users.noreply.github.com>
2023-06-28 18:01:45 +0300

    Update FirstTask.cs

--- task_1/FirstTask.cs
+++ task_1/FirstTask.cs
@@ -1,13 +1,32 @@
-namespace Library
+﻿namespace Library
 {
     public class ZadanieOne
     {
         public void One()
         {
             Console.Write("Введите число N: ");
-            int n = int.Parse(Console.ReadLine());
-            string result = string.Join(", ", Enumerable.Range(1, n));
-            Console.WriteLine(result);
+            int endValue;
+            bool isValidInput = int.TryParse(Console.ReadLine(), out endValue);
+
+            while (!isValidInput || endValue <= 0)
+            {
+                Console.WriteLine("Некорректный ввод. Введите положительное целое число.");
+                isValidInput = int.TryParse(Console.ReadLine(), out endValue);
+            }
+            PrintSequence(1, endValue);
+        }
+        private void PrintSequence(int startValue, int endValue)
+        {
+            for (int i = startValue; i <= endValue; i++)
+            {
+                Console.Write(i);
+
+                if (i < endValue)
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine(".");
         }
     }
     public class ZadanieTwo
@@ -15,16 +34,32 @@ namespace Library
         public static void Two()
         {
             Console.Write("Введите нечетное число N: ");
-            int n = int.Parse(Console.ReadLine());
-            if (n % 2 == 0) // If N is an even number, exit the function.
+
+            int size;
+            bool isValidInput = int.TryParse(Console.ReadLine(), out size);
+
+            while (!isValidInput || size < 3 || size % 2 == 0)
             {
-                Console.WriteLine("Вы ввели четное N.");
-                return;
-            }            
-            for (int row = 1; row <= n; row++) {
-                for (int col = 1; col <= n; col++) {
-                    if (row == n / 2 + 1 && col == n / 2 + 1) Console.Write(" ");
-                    else Console.Write("#");                    
+                Console.WriteLine("Некорректный ввод. Введите нечетное число, большее 3.");
+                isValidInput = int.TryParse(Console.ReadLine(), out size);
+            }
+            DrawSquare(size);
+        }
+
+        static void DrawSquare(int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (i == size / 2 && j == size / 2)
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write("#");
+                    }
                 }
                 Console.WriteLine();
             }
