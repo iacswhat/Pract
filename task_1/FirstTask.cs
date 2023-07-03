8506abd2812bd3dae581473304be03d77ba13b90
iacswhat <111007847+iacswhat@users.noreply.github.com>
2023-07-03 14:32:40 +0300

    Update FirstTask.cs

--- task_1/FirstTask.cs
+++ task_1/FirstTask.cs
@@ -1,52 +1,61 @@
 ﻿namespace Library
 {
-    public class ZadanieOne
+
+    public class Parser
     {
-        public void One()
+        public static int ParseInt(int task)
         {
-            Console.Write("Введите число N: ");
             int endValue;
             bool isValidInput = int.TryParse(Console.ReadLine(), out endValue);
-
-            while (!isValidInput || endValue <= 0)
+            
+            switch (task)
             {
-                Console.WriteLine("Некорректный ввод. Введите положительное целое число.");
-                isValidInput = int.TryParse(Console.ReadLine(), out endValue);
+                case 1:
+                    while (!isValidInput || endValue <= 1)
+                    {
+                        Console.WriteLine("Некорректный ввод.");
+                        isValidInput = int.TryParse(Console.ReadLine(), out endValue);
+                    }
+                    break;
+
+                case 2:
+                    while (!isValidInput || endValue < 3 || endValue % 2 == 0)
+                    {
+                        Console.WriteLine("Некорректный ввод.");
+                        isValidInput = int.TryParse(Console.ReadLine(), out endValue);
+                    }
+                    break;
             }
+            return endValue;
+        }
+    }
+
+    public class TaskOne
+    {
+        public static void One()
+        {
+            Console.Write("Введите число N: ");
+            int endValue = Parser.ParseInt(1);
             PrintSequence(1, endValue);
         }
-        private void PrintSequence(int startValue, int endValue)
-        {
-            for (int i = startValue; i <= endValue; i++)
-            {
-                Console.Write(i);
 
-                if (i < endValue)
-                {
-                    Console.Write(", ");
-                }
-            }
+        private static void PrintSequence(int startValue, int endValue)
+        {
+            Console.Write(string.Join(", ", Enumerable.Range(1, endValue)));
             Console.WriteLine(".");
         }
     }
-    public class ZadanieTwo
+
+    public class TaskTwo
     {
         public static void Two()
         {
-            Console.Write("Введите нечетное число N: ");
-
-            int size;
-            bool isValidInput = int.TryParse(Console.ReadLine(), out size);
-
-            while (!isValidInput || size < 3 || size % 2 == 0)
-            {
-                Console.WriteLine("Некорректный ввод. Введите нечетное число, большее 3.");
-                isValidInput = int.TryParse(Console.ReadLine(), out size);
-            }
+            Console.Write("Введите нечетное число N больше 3: ");
+            int size = Parser.ParseInt(2);
             DrawSquare(size);
         }
 
-        static void DrawSquare(int size)
+        private static void DrawSquare(int size)
         {
             for (int i = 0; i < size; i++)
             {
